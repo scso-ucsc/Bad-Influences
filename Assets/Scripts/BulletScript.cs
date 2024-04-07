3525e90c3f10f0f6182c7cdbe3b0065503aecf5b
scso-ucsc <scso@ucsc.edu>
2024-04-07 16:49:56 -0700

    Adding Zombie Prefab

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -8,11 +8,10 @@ public class BulletScript : MonoBehaviour
         if(collider.gameObject.tag == "Wall"){ //If bullet collides with a wall, disable the bulett
             this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero; //Resetting velocity to 0 so that if active again, that force no longer applies
             this.gameObject.SetActive(false);
-        } 
-        // else if(collider.gameObject.tag == "Zombie"){
-        //     collider.gameObject.SetActive(false);
-        //     this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero; //Resetting velocity to 0 so that if active again, that force no longer applies
-        //     this.gameObject.SetActive(false);
-        // }
+        } else if(collider.gameObject.tag == "Zombie"){
+            //collider.GameObject.hurt();
+            this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero; //Resetting velocity to 0 so that if active again, that force no longer applies
+            this.gameObject.SetActive(false);
+        }
     }
 }
---
+++ Assets/Scripts/ZombieScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieScript : MonoBehaviour
+{
+    //Zombie Variables
+    private int zombieHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        zombieHealth = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collider){
+        if(collider.gameObject.tag == "BasicBullet"){ //If zombie gets hit by bullet, reduce health
+            Debug.Log("I am hit");
+            zombieHealth -= 4;
+        }
+        if(zombieHealth <= 0){
+                zombieHealth = 10; //Resetting Health
+                this.gameObject.SetActive(false); //Deactivating Zombie
+        }
+    }
+
+    public static void hurt(){
+        Debug.Log("I am hit");
+    }
+}
