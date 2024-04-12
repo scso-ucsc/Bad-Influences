268b97c0b163e613c9377fe1d4266f304afeb3d3
Emil Sabev <esabev@ucsc.edu>
2024-04-12 12:02:31 -0700

    test commit

--- Assets/Managers/UIManager.cs
+++ Assets/Managers/UIManager.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro; //Needed for using TextMeshPro
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
     public static UIManager instance;
     [SerializeField] private TextMeshProUGUI playerScoreText;
+    [SerializeField] private GameObject gameoverUI;
 
     void Awake(){
         if(instance == null){
@@ -19,12 +21,22 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameoverUI.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         playerScoreText.text = "Enemies Defeated: " + GameManager.instance.getPlayerScore().ToString(); //Updating score constantly
+
+        if (GameManager.instance.returnGameOverStatus() == true)
+        {
+            gameoverUI.SetActive(true);
+        }
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene("PlayScene");
     }
 }
