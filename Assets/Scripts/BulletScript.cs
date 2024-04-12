c000d878b67692e95f4a2700017dd89bce145bbc
Jayc <116532978+JaydenChung@users.noreply.github.com>
2024-04-12 11:31:47 -0700

    fixed_headshots

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -5,17 +5,33 @@ using UnityEngine;
 public class BulletScript : MonoBehaviour
 {
     [SerializeField] private string bulletType;
-    private void OnCollisionEnter(Collision collider){
-        if(collider.gameObject.tag == "Wall"){ //If bullet collides with a wall, disable the bulett
-            this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero; //Resetting velocity to 0 so that if active again, that force no longer applies
-            this.gameObject.SetActive(false);
-        } else if(collider.gameObject.tag == "Zombie"){
-            var colliderScript = collider.gameObject.GetComponent<ZombieScript>(); //Accessing Zombie Script of Zombie Object
-            if(bulletType == "basic"){
-                colliderScript.zombieHurt(4);
-            }
-            this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero; //Resetting velocity to 0 so that if active again, that force no longer applies
-            this.gameObject.SetActive(false);
+    [SerializeField] private int basicDamage = 4; // Basic damage for body shots
+    [SerializeField] private int headshotMultiplier = 3; // Multiplier for headshots
+
+    private void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.tag == "Wall"){
+            DisableBullet();
+        } else if(collision.gameObject.tag == "Zombie"){
+            ProcessHit(collision.gameObject, basicDamage); // Handle body shot
+        } else if(collision.gameObject.tag == "HeadCollider"){
+        int headshotDamage = basicDamage * headshotMultiplier;
+        GameObject zombieParent = collision.gameObject.transform.parent.gameObject; // Reference to the Zombie GameObject
+        Debug.Log("Headshot detected on " + zombieParent.name + " with damage: " + headshotDamage);
+        ProcessHit(zombieParent, headshotDamage); // Apply damage to the Zombie GameObject
+    }
+    }
+
+    private void DisableBullet(){
+        this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        this.gameObject.SetActive(false);
+    }
+
+    private void ProcessHit(GameObject zombie, int damage){
+        Debug.Log("Damage Applied: " + damage); // Debug the damage being applied
+        var zombieScript = zombie.GetComponent<ZombieScript>();
+        if(zombieScript != null){
+            zombieScript.zombieHurt(damage);
         }
+        DisableBullet();
     }
 }
